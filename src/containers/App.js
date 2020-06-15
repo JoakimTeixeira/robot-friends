a42637656becea23f2bc9de195ba77c5cd04dcc5
Joakim Teixeira <setokim1@hotmail.com>
2020-06-15 17:59:38 -0300

    Refactor code

--- src/containers/App.js
+++ src/containers/App.js
@@ -23,13 +23,13 @@ class App extends Component {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name
-        .toLowerCase()
-        .includes(this.state.searchField.toLowerCase());
+    const { robots, searchField } = this.state;
+
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    if (this.state.robots.length === 0) {
+    if (robots.length === 0) {
       return <h1 className="loading">Loading...</h1>;
     } else {
       return (
