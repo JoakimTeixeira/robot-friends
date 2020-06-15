a4a19edb4347a66a36656534263a8adf4ed3bba5
Joakim Teixeira <setokim1@hotmail.com>
2020-06-15 18:42:49 -0300

    Updates

---
+++ precache-manifest.8ec75cbf825dff943161d13042e13e34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42c8826ddf7cc5a6b3f294331054810",
+    "url": "/RobotFriends/index.html"
+  },
+  {
+    "revision": "6ace6cb5ddb459a7524e",
+    "url": "/RobotFriends/static/css/main.3578d609.chunk.css"
+  },
+  {
+    "revision": "8ac66cf85c535b6f0889",
+    "url": "/RobotFriends/static/js/2.a8a83e8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RobotFriends/static/js/2.a8a83e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ace6cb5ddb459a7524e",
+    "url": "/RobotFriends/static/js/main.0e15b4bb.chunk.js"
+  },
+  {
+    "revision": "348615e84ee6c22e1803",
+    "url": "/RobotFriends/static/js/runtime-main.b71dee6b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
