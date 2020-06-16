d0c5c3b7f8bbcb1653bb7deb45747fdbbb626594
Joakim Teixeira <setokim1@hotmail.com>
2020-06-16 10:28:49 -0300

    Fix search-box responsivity

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,18 +3,16 @@ import "./SearchBox.css";
 
 const SearchBox = ({ searchChange }) => {
   return (
-    <div className="search">
-      <label className="search-title">
-        <h2>Search robots:</h2>
+    <label className="search">
+      <h2 className="search-title">Search robots:</h2>
 
-        <input
-          className="search-input"
-          type="search"
-          placeholder="Insert name"
-          onChange={searchChange}
-        ></input>
-      </label>
-    </div>
+      <input
+        className="search-input"
+        type="search"
+        placeholder="Insert name"
+        onChange={searchChange}
+      ></input>
+    </label>
   );
 };
 
