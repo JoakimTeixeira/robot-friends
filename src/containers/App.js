e44c7b868b28a4ad74487675e46ed0eea22dfc7d
Joakim Teixeira <setokim1@hotmail.com>
2020-10-21 19:16:49 -0300

    Change class component to react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,82 +1,76 @@
-import React, { Fragment, Component } from "react";
-import CardList from "../components/CardList";
-import SearchBox from "../components/SearchBox";
-import MessageError from "../components/MessageError";
-import "./App.css";
+import React, { Fragment, useState, useEffect } from 'react'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import MessageError from '../components/MessageError'
+import './App.css'
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: "",
-      error: false,
-    };
-  }
+const App = () => {
+	const [robots, setRobots] = useState([])
+	const [searchField, setSearchField] = useState('')
+	const [error, setError] = useState(false)
 
-  timeoutPromise(ms, promise) {
-    return new Promise((resolve, reject) => {
-      const timeoutId = setTimeout(() => {
-        reject(new Error());
-      }, ms);
-      promise.then(
-        res => {
-          clearTimeout(timeoutId);
-          resolve(res);
-        },
-        err => {
-          clearTimeout(timeoutId);
-          reject(err);
-        }
-      );
-    });
-  }
+	const timeoutPromise = (ms, promise) => {
+		return new Promise((resolve, reject) => {
+			const timeoutId = setTimeout(() => {
+				reject(new Error())
+			}, ms)
 
-  componentDidMount() {
-    this.timeoutPromise(
-      5000,
-      fetch("https://jsonplaceholder.typicode.com/users")
-        .then(response => response.json())
-        .then(users => this.setState({ robots: users }))
-    ).catch(err => {
-      console.log("HTTP request error", err);
-      this.setState({ error: true });
-    });
-  }
+			promise.then(
+				(res) => {
+					clearTimeout(timeoutId)
+					resolve(res)
+				},
+				(err) => {
+					clearTimeout(timeoutId)
+					reject(err)
+				}
+			)
+		})
+	}
 
-  onSearchChange = event => {
-    this.setState({ searchField: event.target.value });
-  };
+	useEffect(() => {
+		timeoutPromise(
+			5000,
+			fetch('https://jsonplaceholder.typicode.com/users')
+				.then((response) => response.json())
+				.then((users) => setRobots(users))
+		).catch((err) => {
+			console.log('HTTP request error', err)
+			setError(true)
+		})
+	}, [])
 
-  render() {
-    const { robots, searchField, error } = this.state;
+	const onSearchChange = (event) => {
+		setSearchField(event.target.value)
+	}
 
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    });
+	console.log(robots, searchField, error)
 
-    if (robots.length === 0) {
-      return <h1 className="loading">Loading...</h1>;
-    }
+	const filteredRobots = robots.filter((robot) => {
+		return robot.name.toLowerCase().includes(searchField.toLowerCase())
+	})
 
-    if (error) {
-      return <MessageError />;
-    }
+	if (robots.length === 0) {
+		return <h1 className="loading">Loading...</h1>
+	}
 
-    return (
-      <Fragment>
-        <main className="main">
-          <header className="header">
-            <h1 className="header-title">RobotFriends</h1>
-            <SearchBox searchChange={this.onSearchChange} />
-          </header>
-          <section className="card-list">
-            <CardList robots={filteredRobots} />
-          </section>
-        </main>
-      </Fragment>
-    );
-  }
+	if (error) {
+		return <MessageError />
+	}
+
+	return (
+		<Fragment>
+			<main className="main">
+				<header className="header">
+					<h1 className="header-title">RobotFriends</h1>
+					<SearchBox searchChange={onSearchChange} />
+				</header>
+				<section className="card-list">
+					<CardList robots={filteredRobots} />
+				</section>
+			</main>
+		</Fragment>
+	)
 }
 
-export default App;
+export default App
