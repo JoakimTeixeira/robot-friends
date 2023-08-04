53122a18876fcf014e34894dac7b898b31e84940
Joakim Teixeira <setokim1@hotmail.com>
2023-08-04 07:25:33 +0100

    Run prettier on all src files

--- src/containers/index.jsx
+++ src/containers/index.jsx
@@ -1,21 +1,24 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from 'react-redux';
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import { createLogger } from 'redux-logger';
-import thunkMiddleware from 'redux-thunk';
-import App from '../App';
-import { requestRobots, searchRobots } from '../reducers';
-import './index.css';
+import { Provider } from "react-redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import App from "../App";
+import { requestRobots, searchRobots } from "../reducers";
+import "./index.css";
 
-const rootReducer = combineReducers({ searchRobots, requestRobots })
-const logger = createLogger()
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const logger = createLogger();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger),
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>
+  </StrictMode>,
 );
