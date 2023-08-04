4f302093edda0fd95576cc71614799e8803c0155
Joakim Teixeira <setokim1@hotmail.com>
2023-08-04 07:08:39 +0100

    Apply tree shaking to imports

--- src/containers/index.jsx
+++ src/containers/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { Provider } from 'react-redux';
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
@@ -12,10 +12,10 @@ const rootReducer = combineReducers({ searchRobots, requestRobots })
 const logger = createLogger()
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </StrictMode>
 );
